29e65322bc979542542beb01154e54d74fd2b22b
RuGe <70239531+ruge0326@users.noreply.github.com>
2020-12-15 22:09:21 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+def _sum_digit(number):
+    output = ''
+    accumulated = 0
+    year = str(number)
+    for j in year:
+        accumulated += int(j)
+        output += str(j) if output == '' else ' + ' + str(j)
+    if accumulated > 9:
+        return '= ' + output + ' =>' + _sum_digit(accumulated)
+    return '= ' + output + ' = ' + str(accumulated)
+
+
+print('Magic [2020, 2050]')
+for i in range(2020, 2050):
+    magic = _sum_digit(i)
+    print(magic)
